8ac24168a9f8bcf2685d945931de06f94fa694e7
Roman Janovsky <roman.janovsky9@seznam.cz>
2020-02-29 22:15:28 +0100

    Added resource generation for command names.

--- CommandGenerator/CommandGenerator/AddObjectCommand.cs
+++ CommandGenerator/CommandGenerator/AddObjectCommand.cs
@@ -1,10 +1,15 @@
-public class AddObjectCommand
+namespace CommandGenerator
 {
-	public string DisplayName => "Add object";
-	public virtual string ChangedObject { get; set; }// Mapping manytoone
-	public virtual object RandomValue { get; set; }// Mapping 
+
+	public class AddObjectCommand
+	{
+		public string DisplayName => CommandResources.AddObjectCommandName;
+		public virtual string ChangedObject { get; set; }// Mapping manytoone
+		public virtual object RandomValue { get; set; }// Mapping 
 
 
-	protected AddObjectCommand() {}
-	public AddObjectCommand(string test) {}
+		protected AddObjectCommand() {}
+		public AddObjectCommand(string test) {}
+	}
+
 }
--- CommandGenerator/CommandGenerator/SetMultipleValuesCommand.cs
+++ CommandGenerator/CommandGenerator/SetMultipleValuesCommand.cs
@@ -1,11 +1,16 @@
-public class SetMultipleValuesCommand
+namespace CommandGenerator
 {
-	public string DisplayName => "Set values";
-	public virtual string ChangedObject { get; set; }// Mapping ManyToOne
-	public virtual double OldValue { get; set; }// Mapping Property
-	public virtual double NewValue { get; set; }// Mapping property
+
+	public class SetMultipleValuesCommand
+	{
+		public string DisplayName => CommandResources.SetMultipleValuesCommandName;
+		public virtual string ChangedObject { get; set; }// Mapping ManyToOne
+		public virtual double OldValue { get; set; }// Mapping Property
+		public virtual double NewValue { get; set; }// Mapping property
 
 
-	protected SetMultipleValuesCommand() {}
-	public SetMultipleValuesCommand(string test) {}
+		protected SetMultipleValuesCommand() {}
+		public SetMultipleValuesCommand(string test) {}
+	}
+
 }
